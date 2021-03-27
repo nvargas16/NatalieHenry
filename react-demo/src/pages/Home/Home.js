0bced0276d51d78fe683da81edf83866ff177131
nvargas16 <78758622+nvargas16@users.noreply.github.com>
2021-03-26 19:18:24 -0500

    added home page

---
+++ react-demo/src/pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css'
+
+function Home(){
+    return(
+        <div className="root">
+            <h1>Home Page!!!!!!!!!!!</h1>
+            <p>Intro to React</p>
+        </div>
+    )
+}
+
+export default Home;
